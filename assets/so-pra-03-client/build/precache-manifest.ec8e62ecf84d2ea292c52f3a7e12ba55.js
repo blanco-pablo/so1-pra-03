576bac536435745db53503559fa245bed3422e9e
Tikiram Samaneb <tikiram.samaneb@gmail.com>
2019-10-06 18:38:21 -0600

    update

---
+++ assets/so-pra-03-client/build/precache-manifest.ec8e62ecf84d2ea292c52f3a7e12ba55.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "26d5a93e6cc8904d62b5d09e58d8ca50",
+    "url": "/client/index.html"
+  },
+  {
+    "revision": "0e3f2662a2c7dc0a9b7e",
+    "url": "/client/static/css/2.802837ff.chunk.css"
+  },
+  {
+    "revision": "09e1700efd3fcabdcf0b",
+    "url": "/client/static/css/main.d9fff99a.chunk.css"
+  },
+  {
+    "revision": "0e3f2662a2c7dc0a9b7e",
+    "url": "/client/static/js/2.1160f337.chunk.js"
+  },
+  {
+    "revision": "09e1700efd3fcabdcf0b",
+    "url": "/client/static/js/main.c3aab9ff.chunk.js"
+  },
+  {
+    "revision": "044935744696d7d67282",
+    "url": "/client/static/js/runtime-main.3fab3101.js"
+  }
+]);
